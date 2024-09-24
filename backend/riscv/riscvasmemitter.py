9a484ad3067885420f6d6615656baabb54445dc5
liuyd21 <liuyd21@mails.tsinghua.edu.cn>
2024-09-24 09:24:12 +0000

    将不能只用一句汇编代码的运算符写成多个riscv instruction的组合

--- backend/riscv/riscvasmemitter.py
+++ backend/riscv/riscvasmemitter.py
@@ -86,8 +86,25 @@ class RiscvAsmEmitter():
             For different tac operation, you should translate it to different RiscV code
             A tac operation may need more than one RiscV instruction
             """
-            if instr.op == TacBinaryOp.LOR:
+            if instr.op == TacBinaryOp.LOR: #||
                 self.seq.append(Riscv.Binary(RvBinaryOp.OR, instr.dst, instr.lhs, instr.rhs))
+                self.seq.append(Riscv.Unary(RvUnaryOp.SNEZ, instr.dst, instr.dst))
+            elif instr.op == TacBinaryOp.AND: #&&
+                self.seq.append(Riscv.Unary(RvUnaryOp.SNEZ, instr.dst, instr.lhs))
+                self.seq.append(Riscv.Binary(RvBinaryOp.SUB, instr.dst, Riscv.ZERO, instr.dst))
+                self.seq.append(Riscv.Binary(RvBinaryOp.AND, instr.dst, instr.dst, instr.rhs))
+                self.seq.append(Riscv.Unary(RvUnaryOp.SNEZ, instr.dst, instr.dst))
+            elif instr.op == TacBinaryOp.LEQ: #<=
+                self.seq.append(Riscv.Binary(RvBinaryOp.SGT, instr.dst, instr.lhs, instr.rhs))
+                self.seq.append(Riscv.Unary(RvUnaryOp.SEQZ, instr.dst, instr.dst))
+            elif instr.op == TacBinaryOp.GEQ: #>=
+                self.seq.append(Riscv.Binary(RvBinaryOp.SLT, instr.dst, instr.lhs, instr.rhs))
+                self.seq.append(Riscv.Unary(RvUnaryOp.SEQZ, instr.dst, instr.dst))
+            elif instr.op == TacBinaryOp.EQU: #==
+                self.seq.append(Riscv.Binary(RvBinaryOp.SUB, instr.dst, instr.lhs, instr.rhs))
+                self.seq.append(Riscv.Unary(RvUnaryOp.SEQZ, instr.dst, instr.dst))
+            elif instr.op == TacBinaryOp.NEQ: #!=
+                self.seq.append(Riscv.Binary(RvBinaryOp.SUB, instr.dst, instr.lhs, instr.rhs))
                 self.seq.append(Riscv.Unary(RvUnaryOp.SNEZ, instr.dst, instr.dst))
             else:
                 op = {
@@ -98,12 +115,6 @@ class RiscvAsmEmitter():
                     TacBinaryOp.MOD: RvBinaryOp.REM,
                     TacBinaryOp.SLT: RvBinaryOp.SLT,
                     TacBinaryOp.SGT: RvBinaryOp.SGT,
-                    TacBinaryOp.LEQ: RvBinaryOp.SLE,
-                    TacBinaryOp.GEQ: RvBinaryOp.SGE,
-                    TacBinaryOp.NEQ: RvBinaryOp.BNE,
-                    TacBinaryOp.EQU: RvBinaryOp.SEQZ,
-                    TacBinaryOp.AND: RvBinaryOp.AND,
-                    TacBinaryOp.LOR: RvBinaryOp.LOR,
                     # You can add binary operations here.
                 }[instr.op]
                 self.seq.append(Riscv.Binary(op, instr.dst, instr.lhs, instr.rhs))
